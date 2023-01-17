7cc1b3c336b922c5ab0cb0194d20f953152589dd
Gloria Molina <gloriamolina44@gmail.com>
2023-01-17 15:03:15 +0100

    tot

---
+++ codi/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container style="height:100vh" fill-height>
+    <v-row align="center" justify="center" style="height:100%">
+      <v-col align="center">
+        <div style="font-weight:100;font-size:40px;color:white">
+          <strong>En Contrucción</strong> 
+          <v-img src="https://th.bing.com/th/id/OIP.ywoe9UsI3g2vLNUhhr-BaQHaE7?w=270&h=180&c=7&r=0&o=5&dpr=2.5&pid=1.7"></v-img> 
+        </div>
+      </v-col>
+    </v-row>
+  </v-container> 
+</template>
