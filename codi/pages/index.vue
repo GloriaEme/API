3135679402b3da9fe88a3a97106839e61a37960c
Gloria Molina <gloriamolina44@gmail.com>
2023-01-17 19:02:43 +0100

    fin

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -2,8 +2,19 @@
   <v-container>
     <v-row>
       <v-col>
+        <v-text-field v-model="missatge" label="Missatge"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field v-model="usuari" label="Usuari"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn @click="enviarMissatge()">enviar</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <h1 style="font-size:10vh">{{ultimMissatge}}</h1>
-        <h4>{{ultimUsuari}}</h4>  
+        <h4>{{ultimUsuari}}</h4>
       </v-col>
     </v-row>
   </v-container>
@@ -12,15 +23,15 @@
   <script>
   export default{
     mounted(){
-      console.log("abans de descarregar")
-      this.descarregarUltimText()
-      console.log("després de descarregar")
+        this.descarregarUltimText()
     },
     data(){
       return{
         respostaTest:null,
         ultimMissatge:"",
-        ultimUsuari:""
+        ultimUsuari:"",
+        missatge:"",
+        usuari:""
       }
     },
     methods:{
@@ -48,7 +59,7 @@
         this.$axios.get('https://last-one-smo-4w3qk.ondigitalocean.app/api/getText')
         .then(
           r=>{
-            console.log('Ultim text',r)
+            //console.log('Ultim text',r)
             // Recollim
             let missatge = r.data.data.missatge
             let usuari = r.data.data.usuari
@@ -62,6 +73,31 @@
             console.log('Ultim text error',e)
           }
         )
+      },
+      enviarMissatge(){
+        let self = this
+        let dades = {
+          missatge:this.missatge,
+          usuari:this.usuari
+        }
+        let url = 'https://last-one-smo-4w3qk.ondigitalocean.app/api/setText'
+        this.$axios.post(url,dades)
+          .then(
+            r=>{
+              console.log("S'ha enviat bé",r)
+              if(r.data.data.resultat == "fail"){
+                alert(r.data.data.error)
+              }else{
+                self.missatge = ""
+                self.descarregarUltimText()
+              }
+            }
+          )
+          .catch(
+            e=>{
+              console.log("No s'ha enviat bé",e)
+            }
+          )
       }
     }
   }
